0fbc2b2008c10ca8348191c96f383efc9b5f8a1d
waxir <danishdarman06@gmail.com>
2026-03-19 23:48:57 +0500

    image sho is commited

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-async function getUser(){
-    try {
-        let res = await fetch("https://jsonplaceholder.typicode.com/photos");
-        if(!res.ok) throw new Error("Faild to fatch")
+async function getUser() {
+  try {
+    let res = await fetch("https://jsonplaceholder.typicode.com/photos");
+    if (!res.ok) throw new Error("Faild to fatch");
 
-            const data = await res.json()
-            console.log(data)
-            data.texTContent = data;
-    } catch (error) {
-        console.log(error.massage)
-    }
+    const data = await res.json();
+    const mainContainer = document.getElementById("main-container");
+    if(!mainContainer) throw new Error('container not found')
+    data.slice(0, 10).forEach((photo) => {
+        console.log(photo)
+      const img = document.createElement("img");
+      img.src = photo.thumbnailUrl;
+      img.alt = photo.title;
+      img.style.margin = "10px";
+      img.style.borderRadius = "8px";
+      mainContainer.appendChild(img);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-getUser()
-
-
+getUser();
