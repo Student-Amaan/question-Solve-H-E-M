f9b08eb320cc0d20f6e3659008a19c1ad2065557
waxir <danishdarman06@gmail.com>
2026-03-21 20:21:56 +0500

    api commited by button

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
-async function getUser() {
+async function getImages() {
   try {
-    let res = await fetch("https://jsonplaceholder.typicode.com/photos");
-    if (!res.ok) throw new Error("Faild to fatch");
+    const res = await fetch("https://picsum.photos/v2/list");
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch images");
+    }
 
     const data = await res.json();
-    const mainContainer = document.getElementById("main-container");
-    if(!mainContainer) throw new Error('container not found')
+    const container = document.getElementById("main-container");
+
+    if (!container) {
+      throw new Error("Container not found");
+    }
+
+    container.innerHTML = "";
+
     data.slice(0, 10).forEach((photo) => {
-        console.log(photo)
       const img = document.createElement("img");
-      img.src = photo.thumbnailUrl;
-      img.alt = photo.title;
-      img.style.margin = "10px";
-      img.style.borderRadius = "8px";
-      mainContainer.appendChild(img);
+
+      img.src = photo.download_url;
+      img.alt = "Random Image";
+      img.style.height = '200px'
+      img.style.width = '200px'
+
+      container.appendChild(img);
     });
   } catch (error) {
-    console.log(error.message);
+    console.log("Error:", error.message);
   }
 }
 
-getUser();
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("btn");
+
+  if (!btn) {
+    console.log("Button not found");
+    return;
+  }
+
+  btn.addEventListener("click", () => {
+    console.log("Loading images...");
+    getImages();
+  });
+});
